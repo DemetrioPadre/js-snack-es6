57918c8e5fe8ce48df932037d7c7d3353fc74084
DemetrioPadre <demetriopadre@gmail.com>
2024-01-25 14:40:21 +0100

    add array snack 3

---
+++ snack 3/main.js
@@ -0,0 +1,27 @@
+const bicycles = [
+    {
+        name: 'corsa',
+        weight: 5,
+    },
+    {
+        name: 'mtBike',
+        weight: 10,
+    },
+    {
+        name: 'ibrida',
+        weight: 12,
+    }, {
+        name: 'pieghevole',
+        weight: 11,
+    }, {
+        name: 'graziella',
+        weight: 7,
+    }, {
+        name: 'bmx',
+        weight: 8,
+    }, {
+        name: 'freeStyle',
+        weight: 6,
+    },
+];
+console.table(bicycles);
